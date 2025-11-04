9b5ae65ba04ba9762c670b8480949d36458873d2
fdojurado <fdojurado@users.noreply.github.com>
2025-11-04 13:55:00 +0000

    deploy: fdojurado/academic-profile@556a45b4cd8fedf81b3facfdeeb6050568508462

--- html/dynamic-citation-counter.js
+++ html/dynamic-citation-counter.js
@@ -1,13 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   const citationCounter = document.getElementById("citations");
+  const hindexCounter = document.getElementById("hindex");
+  const i10indexCounter = document.getElementById("i10index");
   let count = 0;
+  let hcount = 0;
+  let i10count = 0;
   // end is the current number of citations (id="citations")
   const end = parseInt(citationCounter.innerText, 10);
+  const hend = parseInt(hindexCounter.innerText, 10);
+  const i10end = parseInt(i10indexCounter.innerText, 10);
   citationCounter.innerText = "0";
+  hindexCounter.innerText = "0";
+  i10indexCounter.innerText = "0";
 
   const counter = setInterval(() => {
     count++;
     citationCounter.innerText = count;
     if (count >= end) clearInterval(counter);
   }, 20);
+  const hcounter = setInterval(() => {
+    hcount++;
+    hindexCounter.innerText = hcount;
+    if (hcount >= hend) clearInterval(hcounter);
+  }, 200);
+  const i10counter = setInterval(() => {
+    i10count++;
+    i10indexCounter.innerText = i10count;
+    if (i10count >= i10end) clearInterval(i10counter);
+  }, 200);
 });
