309fb622ac7a461a9e83a757e0c9884c664cd9f6
fdojurado <fdojurado@users.noreply.github.com>
2025-11-04 15:12:43 +0000

    deploy: fdojurado/academic-profile@b75c32d7857e46fcbc226774fed1c0d317b0ab24

--- html/dynamic-citation-counter.js
+++ html/dynamic-citation-counter.js
@@ -2,16 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const citationCounter = document.getElementById("citations");
   const hindexCounter = document.getElementById("hindex");
   const i10indexCounter = document.getElementById("i10index");
+  const mostCitedCounter = document.getElementById("most-cited");
   let count = 0;
   let hcount = 0;
   let i10count = 0;
+  let mcount = 0;
   // end is the current number of citations (id="citations")
   const end = parseInt(citationCounter.innerText, 10);
   const hend = parseInt(hindexCounter.innerText, 10);
   const i10end = parseInt(i10indexCounter.innerText, 10);
+  const mend = parseInt(mostCitedCounter.innerText, 10);
   citationCounter.innerText = "0";
   hindexCounter.innerText = "0";
   i10indexCounter.innerText = "0";
+  mostCitedCounter.innerText = "0";
 
   const counter = setInterval(() => {
     count++;
@@ -22,10 +26,15 @@ document.addEventListener("DOMContentLoaded", () => {
     hcount++;
     hindexCounter.innerText = hcount;
     if (hcount >= hend) clearInterval(hcounter);
-  }, 200);
+  }, 300);
   const i10counter = setInterval(() => {
     i10count++;
     i10indexCounter.innerText = i10count;
     if (i10count >= i10end) clearInterval(i10counter);
-  }, 200);
+  }, 300);
+  const mcounter = setInterval(() => {
+    mcount++;
+    mostCitedCounter.innerText = mcount;
+    if (mcount >= mend) clearInterval(mcounter);
+  }, 20);
 });
